a1ba330e40e8b54c27bb021eae3e54ce20fb62c9
luanascardua <92794401+luanascardua@users.noreply.github.com>
2022-07-03 13:12:53 -0300

    send email

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
+import os
 
 from scraping import Scraping
-
+from colors import Colors
 
 if __name__ == '__main__':
+
+    os.system('cls')
+
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--log-level=3')
     chrome_options.add_argument('--start-maximized')
 
-    chrome = Service('c:\chromedriver.exe')
+    chrome = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=chrome, options=chrome_options)
-    driver.get('https://telefonesimportados.netlify.app/')
     
     scraping = Scraping(driver)
+    scraping.window_email()
+
+    driver.get('https://telefonesimportados.netlify.app/')
+
     scraping.web_scraping()
-    #scraping.workbook()
+    scraping.workbook()
+    line = 2
+    for nome, preco in zip(scraping.list_nome_phone, scraping.list_preco_phone):
+        scraping.inserir_dados(line, nome, preco)
+        line += 1
+    scraping.wb.save(scraping.file)
+    scraping.send_email()
+
+    print('\n', '_' * 80)
+    input(f'\tPrograma Finalizado. Aperte {Colors.blue} >> ENTER << {Colors.reset} para fechar o navegador')
     driver.quit()
--- scraping.py
+++ scraping.py
@@ -1,15 +1,19 @@
 from email.mime.multipart import MIMEMultipart
 from selenium.webdriver.common.by import By
+from PySimpleGUI import PySimpleGUI as sg
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from openpyxl.styles import Font
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 from datetime import datetime
 from email import encoders
+from tqdm import tqdm
 import smtplib
 import json
 import re
 import os
+
+from colors import Colors
 
 
 class Scraping:
@@ -29,34 +33,39 @@ class Scraping:
         if '«' in pages:
             pages = str(pages).strip('«').strip('»').strip()
 
-        for page in range(1, len(pages)):
+        for page in tqdm (range(1, len(pages))):
+            tqdm.write(f'Página: {page}')
+            os.system('cls')
             for item in range(1, 13):
                 nome_phone = self.driver.find_elements(By.XPATH, f'/html/body/div[5]/div[2]/div[1]/div[{item}]/div/h2/a')
                 preco_phone = self.driver.find_elements(By.XPATH, f'/html/body/div[5]/div[2]/div[1]/div[{item}]/div/div[2]/ins')
                 self.list_nome_phone.append(nome_phone[0].text)
                 self.list_preco_phone.append(preco_phone[0].text)
             self.driver.find_element(*self.btn_next).click()
-        print(self.list_nome_phone)
-        print(self.list_preco_phone)
-
-    def workbook(self, line, nome, preco, file='telefones importados.xlsx'):
+    
+    def workbook(self, file='telefones importados.xlsx'):
         self.file = file
         if not os.path.isfile(self.file):
+            print('não existe')
             self.wb = Workbook()
-            self.ws = self.wb.active
             self.wb.save(self.file)
+        else:
+            self.wb = load_workbook(self.file)
+        self.ws = self.wb.active
 
-        style_column_name = Font(name='Calibri',
-                    size = 11,
-                    bold = True)
-        a1 = self.ws['A1']
-        b1 = self.ws['B1']
-        c1 = self.ws['C1']
-        a1.font = style_column_name
-        b1.font = style_column_name
-        c1.font = style_column_name
+    def inserir_dados(self, line, nome, preco):
 
         if  self.ws.cell(column=1, row=1).value == None:
+            style_column_name = Font(name='Calibri',
+                    size = 11,
+                    bold = True)
+            a1 = self.ws['A1']
+            b1 = self.ws['B1']
+            c1 = self.ws['C1']
+            a1.font = style_column_name
+            b1.font = style_column_name
+            c1.font = style_column_name
+
             self.ws.cell(column=1, row=1).value = 'nome'
             self.ws.cell(column=2, row=1).value = 'preco'
             self.ws.cell(column=3, row=1).value = 'horário'
@@ -65,11 +74,29 @@ class Scraping:
         self.ws.cell(column=2, row=line).value = preco
         self.ws.cell(column=3, row=line).value = datetime.now().strftime('%d/%m/%Y %H:%M')
 
-    def send_email(self, login, password):
+    def window_email(self):
+
+        sg.theme('Reddit')
+        layout = [
+            [sg.Text('gmail '), sg.Input(key='email', size=(35, 1))],
+            [sg.Button('enviar', size=(8))]
+        ]
+        window = sg.Window('Gmail para envio do arquivo', layout, finalize=True)
+        while True:
+            event, self.value = window.read()
+            if event == 'enviar':
+                break
+            elif event == sg.WIN_CLOSED:
+                break
+        window.close()
+        
+    def send_email(self):
 
         with open('server.json', 'r') as f:
             server = json.load(f)
-       
+        login = server['login']
+        password = server['password']
+
         server = smtplib.SMTP(server['host'], server['port'])
         server.ehlo()
         server.starttls()
@@ -77,23 +104,24 @@ class Scraping:
 
         email_msg = MIMEMultipart()
         email_msg['From'] = login
-        email_msg['To'] = ''
-        email_msg['Subject'] = 'Test'
+        email_msg['To'] = self.value['email']
+        email_msg['Subject'] = 'Planilha Phones e-commerce'
 
-        email_msg.attach(MIMEText('', 'html'))
+        email_msg.attach(MIMEText('Olá. <br> Segue anexo arquivo <b>.xlsx</b> com os nomes e preços correspondentes \
+            aos celulares do site: https://telefonesimportados.netlify.app/', 'html'))
 
-        attachment = open('', 'rb')
+        attachment = open(self.file, 'rb')
         att = MIMEBase('application', 'octect-stream')
         att.set_payload(attachment.read())
         encoders.encode_base64(att)
 
-        att.add_header('Content-Disposition', f'attachment; filename=file.pdf')
+        att.add_header('Content-Disposition', f'attachment; filename={self.file}')
         attachment.close()
 
         email_msg.attach(att)
         try:
             server.sendmail(email_msg['From'], email_msg['To'], email_msg.as_string())
             server.quit()
-            print('Email successfully sent.')
+            print('\nEmail successfully sent.')
         except Exception as e:
-            print(f'ERROR: {e}')
+            print(f'{Colors.red}ERROR: {Colors.reset}{e}')
